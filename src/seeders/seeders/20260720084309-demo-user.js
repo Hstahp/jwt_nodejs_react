3d82b60e5ff848296108dbfad9bf1c3705bf24e4
Huy Phatt <phatle219@gmail.com>
2026-07-20 15:52:57 +0700

    feat: generate model/migration/seeder

---
+++ src/seeders/seeders/20260720084309-demo-user.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        await queryInterface.bulkInsert(
+            'Users',
+            [
+                {
+                    username: 'john@doe',
+                    email: 'John Doe',
+                    password: 'fake1',
+                },
+                {
+                    username: 'john2@doe',
+                    email: 'John Doe2',
+                    password: 'fake2',
+                },
+                {
+                    username: 'john3@doe',
+                    email: 'John Doe3',
+                    password: 'fake3',
+                },
+            ],
+            {},
+        );
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('Users', null, {});
+    },
+};
