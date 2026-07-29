c45330eeaaa9b3d23ec38a849df095bac7c97a9f
Huy Phatt <phatle219@gmail.com>
2026-07-29 21:34:33 +0700

    feat: create new user

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -67,7 +67,12 @@ const getUserWithPagination = async (page, limit) => {
 
 const createNewUser = async (data) => {
     try {
-        await db.User.create({});
+        await db.User.create(data);
+        return {
+            EM: 'Create user success',
+            EC: 0,
+            DT: [],
+        };
     } catch (e) {
         console.log(e);
     }
