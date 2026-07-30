ed537446aa7f1ae2379c77a62602ec0b02e02350
Huy Phatt <phatle219@gmail.com>
2026-07-30 16:05:35 +0700

    feat: edit user, complete CRUD

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -139,13 +139,36 @@ const createNewUser = async (data) => {
 
 const updateUser = async (data) => {
     try {
+        if (!data.groupId) {
+            return {
+                EM: 'Error with empty GroupId',
+                EC: 1,
+                DT: 'group',
+            };
+        }
+
         let user = await db.User.findOne({
             where: { id: data.id },
         });
         if (user) {
-            user.save({});
+            await user.update({
+                username: data.username,
+                sex: data.gender,
+                address: data.address,
+                groupId: data.groupId,
+            });
+            return {
+                EM: 'Update user success',
+                EC: 0,
+                DT: '',
+            };
         } else {
             //not found
+            return {
+                EM: 'User not found',
+                EC: 2,
+                DT: [],
+            };
         }
     } catch (e) {
         console.log(e);
