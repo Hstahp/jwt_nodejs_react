717fc1f95a60df04bba0c7e3e4761668c04049eb
Huy Phatt <phatle219@gmail.com>
2026-08-04 21:28:22 +0700

    feat: read, update, delete role

--- src/services/roleApiService.js
+++ src/services/roleApiService.js
@@ -30,6 +30,129 @@ const createNewRoles = async (roles) => {
     }
 };
 
+const getAllRoles = async () => {
+    try {
+        let data = await db.Role.findAll();
+
+        return {
+            EM: 'Get all Roles success',
+            EC: 0,
+            DT: data,
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: 'Something went wrong from service...',
+            EC: -1,
+            DT: [],
+        };
+    }
+};
+
+const getRolesWithPagination = async (page, limit) => {
+    try {
+        let offset = (page - 1) * limit;
+
+        const { count, rows } = await db.Role.findAndCountAll({
+            offset: offset,
+            limit: limit,
+            attributes: ['id', 'url', 'description'],
+            order: [['id', 'DESC']],
+        });
+        let totalPages = Math.ceil(count / limit);
+        let data = {
+            totalRows: count,
+            totalPages: totalPages,
+            roles: rows,
+        };
+        return {
+            EM: 'fetch ok',
+            EC: 0,
+            DT: data,
+        };
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: 'Something wrong with service',
+            EC: -1,
+            DT: [],
+        };
+    }
+};
+
+const updateRole = async (data) => {
+    try {
+        if (!data.id) {
+            return {
+                EM: 'Error with empty id',
+                EC: 1,
+                DT: '',
+            };
+        }
+
+        let role = await db.Role.findOne({
+            where: { id: data.id },
+        });
+        if (role) {
+            await role.update({
+                url: data.url,
+                description: data.description,
+            });
+            return {
+                EM: 'Update role success',
+                EC: 0,
+                DT: '',
+            };
+        } else {
+            //not found
+            return {
+                EM: 'Role not found',
+                EC: 2,
+                DT: [],
+            };
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: 'Something wrong with service',
+            EC: -1,
+            DT: [],
+        };
+    }
+};
+
+const deleteRole = async (id) => {
+    try {
+        let role = await db.Role.findOne({
+            where: { id: id },
+        });
+        if (role) {
+            await role.destroy();
+            return {
+                EM: 'Delete role success',
+                EC: 0,
+                DT: [],
+            };
+        } else {
+            return {
+                EM: 'Role not found',
+                EC: 1,
+                DT: [],
+            };
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            EM: 'Something wrong with service',
+            EC: -1,
+            DT: [],
+        };
+    }
+};
 module.exports = {
     createNewRoles,
+    getAllRoles,
+    getRolesWithPagination,
+    updateRole,
+    deleteRole,
 };
